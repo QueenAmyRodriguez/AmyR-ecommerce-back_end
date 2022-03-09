cc11359ac6322d23d68f9592c2a2ac24ca55e828
Amy Rodriguez <queenamyrodriguez@gmail.com>
2022-03-09 16:44:36 -0700

    fixed association

--- models/index.js
+++ models/index.js
@@ -5,26 +5,25 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 
+// Categories have many Products
+Category.hasMany(Product)
+
 // Products belongsTo Category
 Product.belongsTo(Category, {
   foreignKey: 'category_id'
 });
-
-// Categories have many Products
-Category.hasMany(Product, {
-  foreignKey: 'product_id'
-});
+;
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  as: 'product_tags',
-  through: ProductTag,
+  as: 'products',
+  through: 'product_tag',
   foreignKey: 'product_id'
 });
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  as: 'product_tags',
-  through: ProductTag,
+  as: 'tags',
+  through: 'product_tag',
   foreignKey: 'tag_id'
 });
 
